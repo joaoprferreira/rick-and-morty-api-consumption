922822cbd0c4e271450f26dcf62d70cc0e6703d6
João Pedro <joaof082@gmail.com>
2021-06-21 17:04:40 -0300

    iniciando component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import React from "react"
+
+import Header from "./Components/Header/Header"
+
 function App() {
   return (
     <div className="App">
-      <h1> Hello APP </h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <React.StrictMode>
+      <header> 
+        Component header 
+      </header>
+    </React.StrictMode>
+  )
+}
