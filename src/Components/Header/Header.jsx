418ad492bbfb508d0297482ebbc3b9862b434ce6
João Pedro <joaof082@gmail.com>
2021-06-21 17:31:36 -0300

    Adicionando logo do Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Logo from "../../assets/images/Logo.png"
 
 import './Header.scss';
 
@@ -7,7 +8,11 @@ export default function Header() {
   return (
     <React.StrictMode>
       <header> 
-        Component header 
+        <img className="logoMain" src={Logo} alt="" />
+
+        <div className="TextMain">
+          <h2>Conheça todos os personagens da série.</h2>
+        </div>
       </header>
     </React.StrictMode>
   )
