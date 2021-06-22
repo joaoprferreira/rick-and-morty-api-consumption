289700404884c5431481be8ce622787ea0f20a31
João Pedro <joaof082@gmail.com>
2021-06-22 15:40:21 -0300

    Criando mock para valores do select

---
+++ src/Components/Header/_mock.js
@@ -0,0 +1,12 @@
+export const optionsGenero = [
+  { value: 'female', label: 'Female' },
+  { value: 'male', label: 'Male' },
+  { value: 'genderless', label: 'Gerderless' },
+  { value: 'unknown', label: 'Desconhecido' }
+]
+
+export const optionsStatus= [
+  { value: 'alive', label: 'Alive' },
+  { value: 'dead', label: 'Dead' },
+  { value: 'unknown', label: 'Desconhecido' }
+]
