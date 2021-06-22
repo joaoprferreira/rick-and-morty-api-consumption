69126d9c3b0a3959d51b1214c8d4c6e49a878184
João Pedro <joaof082@gmail.com>
2021-06-22 15:42:25 -0300

    Criação do component select.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-
+import Select from 'react-select'
 import Logo from "../../assets/images/Logo.png";
-
-import Genero from "../Toggle menu/Genero/Genero"
-
+import { Search } from "../../Components/Search/Search"
+import { optionsGenero, optionsStatus } from './_mock';
 import './Header.scss';
 
 export default function Header() {
   return (
-    <React.StrictMode>
-      <header> 
-        <img className="logoMain" src={Logo} alt="" />
-
-        <div className="TextMain">
-          <h2>Conheça todos os personagens da série.</h2>
+    <header>
+      <img className="logoMain" src={Logo} alt="logo rick and morty" />
+      <div className="TextMain">
+        <h2 className='white'>Conheça todos os personagens da série.</h2>
+        <div className="filterArea">
+          <Select options={optionsGenero} placeholder="Genêro"/>
+          <Select options={optionsStatus} placeholder='Status'/>
+          <Search placeholder="Procurar" />
         </div>
-        <Genero />
-      </header>
-    </React.StrictMode>
+      </div>
+    </header>
   )
 }
