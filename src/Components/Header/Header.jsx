9f2fd716b871a3597f24392971620e4e99c5ac29
João Pedro <joaof082@gmail.com>
2021-06-22 23:56:53 -0300

    Criando Filtros no Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Select from 'react-select'
 import Logo from "../../assets/images/Logo.png";
 import { Search } from "../../Components/Search/Search"
@@ -6,15 +6,47 @@ import { optionsGenero, optionsStatus } from './_mock';
 import './Header.scss';
 
 export default function Header() {
+  const [ filters,setFilters] = useState({
+    gender: "",
+    status: "",
+    name: "",
+  })
+
+  const handleChangeFilters = (data) => {
+    setFilters({
+      ...filters,
+      [data.key]: data.value
+    })
+  }
+
+  useEffect(() => {
+    console.log("filters@=",filters)
+  },[filters])
+
+  
   return (
     <header>
       <img className="logoMain" src={Logo} alt="logo rick and morty" />
       <div className="TextMain">
         <h2 className='white'>Conheça todos os personagens da série.</h2>
         <div className="filterArea">
-          <Select options={optionsGenero} placeholder="Genêro"/>
-          <Select options={optionsStatus} placeholder='Status'/>
-          <Search placeholder="Procurar" />
+          <Select
+            options={optionsGenero}
+            placeholder="Genêro" 
+            onChange={({value})=> {
+              handleChangeFilters({ value, key:"gender" })
+            }}
+          />
+          <Select
+            options={optionsStatus}
+            placeholder='Status'
+            onChange={({value})=> {
+              handleChangeFilters({ value, key:"status" })
+            }}
+          />
+          <Search
+            placeholder="Procurar" 
+          />
         </div>
       </div>
     </header>
