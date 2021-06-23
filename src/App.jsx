1fdcc7eb21333436b77257eac1dc896235196b66
João Pedro <joaof082@gmail.com>
2021-06-22 23:54:10 -0300

    Adicionando Home Ao app e implementado uma api fake para teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
-
 import Header from "./Components/Header/Header"
+import Home from "./Containers/Home/Home"
+import { characters } from "./_mock/apiMock";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home characterList={characters}/>
     </div>
   );
 }
