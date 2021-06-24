25dfe26c66545779767a45234ddc4873e5d0dd64
João Pedro <joaof082@gmail.com>
2021-06-23 21:51:39 -0300

    integra filtro com api

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,29 +1,49 @@
 import React, { useEffect, useState } from 'react'
+import { useAppContext } from "../../Contexts/AppContext";
 import Select from 'react-select'
 import Logo from "../../assets/images/Logo.png";
 import { Search } from "../../Components/Search/Search"
 import { optionsGenero, optionsStatus } from './_mock';
 import './Header.scss';
 
+const intialFiltersState = {
+  gender: "",
+  status: "",
+  name: "",
+}
+
 export default function Header() {
-  const [ filters,setFilters] = useState({
-    gender: "",
-    status: "",
-    name: "",
-  })
+  const [filters, setFilters] = useState(intialFiltersState);
+
+  let debounce = null
+
+  const { getByFilter } = useAppContext();
 
   const handleChangeFilters = (data) => {
+    
     setFilters({
       ...filters,
       [data.key]: data.value
     })
   }
 
-  useEffect(() => {
-    console.log("filters@=",filters)
-  },[filters])
+  const handleSearch = (data) => {
+    clearTimeout(debounce)
+    debounce = setTimeout(() => {
+      setFilters({
+        ...filters,
+        [data.key]: data.value
+      })
+    },[600])
+  }
 
-  
+  useEffect(() => {
+    getByFilter(filters)
+  }, [
+    filters,
+    getByFilter
+  ])
+
   return (
     <header>
       <img className="logoMain" src={Logo} alt="logo rick and morty" />
@@ -32,21 +52,26 @@ export default function Header() {
         <div className="filterArea">
           <Select
             options={optionsGenero}
-            placeholder="Genêro" 
-            onChange={({value})=> {
-              handleChangeFilters({ value, key:"gender" })
+            placeholder="Genêro"
+            onChange={({ value }) => {
+              handleChangeFilters({ value, key: "gender" })
             }}
           />
           <Select
             options={optionsStatus}
             placeholder='Status'
-            onChange={({value})=> {
-              handleChangeFilters({ value, key:"status" })
+            onChange={({ value }) => {
+              handleChangeFilters({ value, key: "status" })
             }}
           />
           <Search
-            placeholder="Procurar" 
+            placeholder="Procurar"
+            onChange={({ target: { value, name } }) => {
+              handleSearch({ value, key: "name" })
+            }}
+            value = {filters.name}
           />
+          <button onClick={() => setFilters(intialFiltersState)}>limpar</button>
         </div>
       </div>
     </header>
